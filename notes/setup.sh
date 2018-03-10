3887d5d0c5783f1c01279bcd0b7ae22b7da73099
Tim Uy <tim@loqu8.com>
2018-03-10 12:23:11 -0800

    working on sync

--- notes/setup.sh
+++ notes/setup.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-# Copyright (c) 2015 The MotaCoin developers
+# Copyright (c) 2018 The MotaCoin developers
 echo -e "\033[1;33mSetting up MotaCoin Explorer...\e[0m"
 
+# parameters
 DBNAME="motadb"
 DBUSER="motauser"
 DBPASS="m0T43xp!0re"
+RPCPORT=17421
+RPCUSER="rpcuser"
+RPCPASS="password1"
+EXPTITLE="MotaCoin Explorer"
+EXPCOIN="MotaCoin"
+EXPSYMBOL="MTC"
+EXPUSER="motauser"
+TWITTER="motacoin"
+FACEBOOK="motacoin"
+GOOGLEPLUS="motacoin"
+APIBLOCKINDEX=77
+APIBLOCKHASH="000000cd90599a062472be2e2d16c934aa0972ea968c1455389ff9100a8ca909"
+APITXHASH="e5766229381fba1efb29cc369b253bf39df6e0edcd54a603751f13c3598794b8"
+APIADDRESS="MFGYRrpK5nKC6EfTuJXSbsFqYTKP1CsAjN"
+EXCHANGE="BTC"
+CRYPTOPIAID="1658"
+CCEXKEY="xxx-xxx-xxx-xxx"
+GENTX=""
+GENBLOCK="00000adbafb59ecf24b5d0933cab00b251a155f4a26f184bcb544ae08be3af7b"
 
 defSkip=0
 
@@ -17,16 +37,18 @@ then
   echo -e "\033[0;33mSkipping $SKIP step(s)\e[0m"
 fi
 
+# 1/10
 ((STEP++))
 if [ $STEP -gt $SKIP ]
 then
   echo -e "\033[0;33m$STEP/$NSTEPS Installing nodejs and npm\e[0m"
-  apt install nodejs npm
+  apt install -y nodejs npm
   # https://stackoverflow.com/questions/18130164/nodejs-vs-node-on-ubuntu-12-04
   ln -s `which nodejs` /usr/bin/node
   echo
 fi
 
+# 2/10
 ((STEP++))
 if [ $STEP -gt $SKIP ]
 then
@@ -50,6 +72,7 @@ then
   echo
 fi
 
+# 3/10
 ((STEP++))
 if [ $STEP -gt $SKIP ]
 then
@@ -59,13 +82,133 @@ then
   echo
 fi
 
+# 4/10
 ((STEP++))
 if [ $STEP -gt $SKIP ]
 then
 echo -e "\033[0;33m$STEP/$NSTEPS Installing dependencies\e[0m"
 echo -e "\033[0;33m   - Installing libkrb5-dev for kerberos...\e[0m"
-apt install libkrb5-dev
+apt install -y libkrb5-dev
 echo -e "\033[0;33m   - Installing production dependencies...\e[0m"
 npm install --production
 fi
 
+# 5/10
+((STEP++))
+if [ $STEP -gt $SKIP ]
+then
+  echo -e "\033[0;33m$STEP/$NSTEPS Configuring explorer\e[0m"
+  echo -e "\033[0;33m   - Replicating template...\e[0m"
+  cp ./settings.json.template ./settings.json
+# ideally use jq but sadly jq does not support comments!
+# echo -e "\033[0;33m   - Installing jq...\e[0m"
+# apt install -y jq
+  echo -e "\033[0;33m   - Replacing parameters...\e[0m"
+
+  # General
+  confSearch='"title": ".*"'
+  confReplacement='"title": "'$EXPTITLE'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"coin": ".*"'
+  confReplacement='"coin": "'$EXPCOIN'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"symbol": ".*"'
+  confReplacement='"symbol": "'$EXPSYMBOL'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+
+  # Database
+  confSearch='"user": "iquidus"'
+  confReplacement='"user": "'$DBUSER'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"password": "3xp!0reR"'
+  confReplacement='"password": "'$DBPASS'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"database": ".*"'
+  confReplacement='"database": "'$DBNAME'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+
+  # Wallet
+  confSearch='"port": 9332'
+  confReplacement='"port": '$RPCPORT''
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"user": "darkcoinrpc"'
+  confReplacement='"user": "'$RPCUSER'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"pass": ".*"'
+  confReplacement='"pass": "'$RPCPASS'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+
+  # Social media
+  confSearch='"twitter": ".*"'
+  confReplacement='"twitter": "'$TWITTER'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"facebook": ".*"'
+  confReplacement='"facebook": "'$FACEBOOK'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"googleplus": ".*"'
+  confReplacement='"googleplus": "'$GOOGLEPLUS'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+
+  # API samples
+  confSearch='"blockindex": 1337,'
+  confReplacement='"blockindex": '$APIBLOCKINDEX','
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"blockhash": ".*"'
+  confReplacement='"blockhash": "'$APIBLOCKHASH'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"txhash": ".*"'
+  confReplacement='"txhash": "'$APITXHASH'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"address": "RBiXWscC63Jdn1GfDtRj8hgv4Q6Zppvpwb"'
+  confReplacement='"address": "'$APIADDRESS'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+
+  # Market
+  confSearch='"exchange": ".*"'
+  confReplacement='"exchange": "'$EXCHANGE'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"cryptopia_id": ".*"'
+  confReplacement='"cryptopia_id": "'$CRYPTOPIAID'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"ccex_key": ".*"'
+  confReplacement='"ccex_key": "'$CCEXKEY'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+
+  # Genesis
+  confSearch='"genesis_tx": ".*"'
+  confReplacement='"genesis_tx": "'$GENTX'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json
+  confSearch='"genesis_block": ".*"'
+  confReplacement='"genesis_block": "'$GENBLOCK'"'
+  sed -i'' "s/$confSearch/$confReplacement/g" settings.json 
+fi  
+
+# 6/10
+((STEP++))
+if [ $STEP -gt $SKIP ]
+then
+  echo -e "\033[0;33m$STEP/$NSTEPS Initial sync\e[0m"
+  echo -e "\033[0;33m   - Starting server...\e[0m"
+  npm start &
+  COUNTER=0
+  while !(nc -z localhost 3001) && [[ $COUNTER -lt 60 ]] ; do
+    sleep 2
+    let COUNTER+=2
+    echo -e "\033[0;33m   - Waiting for explorer to initialize... ($COUNTER seconds so far)\e[0m"
+  done
+  echo -e "\033[0;33m   - Updating coin index...\e[0m"
+  nodejs scripts/sync.js index reindex
+  echo -e "\033[0;33m   - Updating market data...\e[0m"
+  nodejs scripts/sync.js market
+  echo -e "\033[0;33m   - Updating peers...\e[0m"
+  nodejs scripts/peers.js
+#  echo -e "\033[0;33m   - Stopping server...\e[0m"
+#  npm stop
+fi
+
+# 7/10
+((STEP++))
+if [ $STEP -gt $SKIP ]
+then
+  echo -e "\033[0;33m$STEP/$NSTEPS Installing cronjobs\e[0m"
+fi
